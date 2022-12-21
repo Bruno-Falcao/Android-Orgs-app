8781c8e5e12d8f90ff23b314310c4ba34384e93f
Bruno-Falcao <brunofalcao321@gmail.com>
2022-12-20 21:00:10 -0300

    Activities implementadas: -Formulario -Produto -lista de produtos

---
+++ app/src/main/java/br/com/alura/orgs/dao/ProdutosDao.kt
@@ -0,0 +1,25 @@
+package br.com.alura.orgs.dao
+
+import br.com.alura.orgs.model.Produto
+import java.math.BigDecimal
+
+class ProdutosDao {
+
+    fun adiciona(produto: Produto){
+        produtos.add(produto)
+    }
+
+    fun buscaTodos() : List<Produto> {
+        return produtos.toList()
+    }
+
+    companion object {
+        private val produtos = mutableListOf<Produto>(
+            Produto(nome = "Salada de frutas",
+             descricao = "Laranja, maçãs e uva",
+             valor = BigDecimal("19.83")
+            )
+        )
+    }
+
+}
---
+++ app/src/main/java/br/com/alura/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
@@ -0,0 +1,60 @@
+package br.com.alura.orgs.ui.recyclerview.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import br.com.alura.orgs.databinding.ProdutoItemBinding
+import br.com.alura.orgs.model.Produto
+import java.text.NumberFormat
+import java.util.Locale
+
+class ListaProdutosAdapter(
+    private val context: Context,
+    produtos: List<Produto>
+) : RecyclerView.Adapter<ListaProdutosAdapter.ViewHolder>() {
+
+    private val produtos = produtos.toMutableList()
+
+    class ViewHolder(private val binding: ProdutoItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun vincula(produto: Produto) {
+            val nome = binding.produtoItemNome
+            nome.text = produto.nome
+
+            val descricao = binding.produtoItemDescricao
+            descricao.text = produto.descricao
+
+            val valor = binding.produtoItemValor
+            formataMoeda(produto)
+            valor.text = "R$ ${produto.valor.toPlainString()}"
+        }
+
+        private fun formataMoeda(produto: Produto): String {
+            val formatador: NumberFormat = NumberFormat.getCurrencyInstance(Locale("pt", "br"))
+            return formatador.format(produto.valor)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(context)
+        val binding = ProdutoItemBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val produto = produtos[position]
+        holder.vincula(produto)
+    }
+
+    override fun getItemCount(): Int = produtos.size
+
+    fun atualiza(produtos: List<Produto>) {
+        this.produtos.clear()
+        this.produtos.addAll(produtos)
+        notifyDataSetChanged()
+    }
+
+}
